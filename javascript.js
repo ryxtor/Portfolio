dad115bd1b75533e7188b23c73deb91f0bf20bcb
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-01-25 20:32:29 -0300

    Change Projects sections from HTML to JavaScript

--- javascript.js
+++ javascript.js
@@ -18,7 +18,7 @@ const project0 = {
   paragraph: `A daily selection of privately personalized reads;
    no accounts or sign-ups required. has been the industry's standard`,
   tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'src/bg-project.svg',
+  image: 'url(src/bg-project.svg)',
   popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
   typesetting industry. Lorem Ipsum has been the industry's standard dummy 
   text ever since the 1500s, when an unknown printer took a galley of type 
@@ -33,7 +33,7 @@ const project1 = {
   paragraph: `A daily selection of privately personalized reads;
    no accounts or sign-ups required. has been the industry's standard`,
   tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'src/bg-project.svg',
+  image: 'url(src/bg-project.svg)',
   popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
   typesetting industry. Lorem Ipsum has been the industry's standard dummy 
   text ever since the 1500s, when an unknown printer took a galley of type 
@@ -48,7 +48,7 @@ const project2 = {
   paragraph: `A daily selection of privately personalized reads;
    no accounts or sign-ups required. has been the industry's standard`,
   tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'src/bg-project.svg',
+  image: 'url(src/bg-project.svg)',
   popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
   typesetting industry. Lorem Ipsum has been the industry's standard dummy 
   text ever since the 1500s, when an unknown printer took a galley of type 
@@ -63,7 +63,7 @@ const project3 = {
   paragraph: `A daily selection of privately personalized reads;
    no accounts or sign-ups required. has been the industry's standard`,
   tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'src/bg-project.svg',
+  image: 'url(src/bg-project.svg)',
   popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
   typesetting industry. Lorem Ipsum has been the industry's standard dummy 
   text ever since the 1500s, when an unknown printer took a galley of type 
@@ -78,7 +78,7 @@ const project4 = {
   paragraph: `A daily selection of privately personalized reads;
    no accounts or sign-ups required. has been the industry's standard`,
   tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'src/bg-project.svg',
+  image: 'url(src/bg-project.svg)',
   popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
   typesetting industry. Lorem Ipsum has been the industry's standard dummy 
   text ever since the 1500s, when an unknown printer took a galley of type 
@@ -93,7 +93,7 @@ const project5 = {
   paragraph: `A daily selection of privately personalized reads;
    no accounts or sign-ups required. has been the industry's standard`,
   tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'src/bg-project.svg',
+  image: 'url(src/bg-project.svg)',
   popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
   typesetting industry. Lorem Ipsum has been the industry's standard dummy 
   text ever since the 1500s, when an unknown printer took a galley of type 
@@ -111,3 +111,89 @@ const projectsArray = [
   project4,
   project5,
 ];
+
+
+
+document.getElementById('projects').innerHTML = `
+<section class="Project-1" id="project-1"> 
+ <h3 class="title-p">${projectsArray[0].title}</h3>
+<p class="p-Project">${projectsArray[0].paragraph}
+</p>
+<ul class="ul-p">
+  <li class="li-p">${projectsArray[0].tecnologies[0]}</li>
+  <li class="li-p">${projectsArray[0].tecnologies[1]}</li>
+  <li class="li-p">${projectsArray[0].tecnologies[2]}</li>
+</ul>
+<button type="button" class="btn-p">See Project</button>
+</section>
+
+ 
+<section class="Project-2" id="project-2"> 
+ <h3 class="title-p">${projectsArray[1].title}</h3>
+<p class="p-Project">${projectsArray[1].paragraph}
+</p>
+<ul class="ul-p">
+  <li class="li-p">${projectsArray[1].tecnologies[0]}</li>
+  <li class="li-p">${projectsArray[1].tecnologies[1]}</li>
+  <li class="li-p">${projectsArray[1].tecnologies[2]}</li>
+</ul>
+<button type="button" class="btn-p">See Project</button>
+</section>
+
+ 
+<section class="Project-3" id="project-3"> 
+ <h3 class="title-p">${projectsArray[2].title}</h3>
+<p class="p-Project">${projectsArray[2].paragraph}
+</p>
+<ul class="ul-p">
+  <li class="li-p">${projectsArray[2].tecnologies[0]}</li>
+  <li class="li-p">${projectsArray[2].tecnologies[1]}</li>
+  <li class="li-p">${projectsArray[2].tecnologies[2]}</li>
+</ul>
+<button type="button" class="btn-p">See Project</button>
+</section>
+
+
+<section class="Project-4" id="project-4"> 
+ <h3 class="title-p">${projectsArray[3].title}</h3>
+<p class="p-Project">${projectsArray[3].paragraph}
+</p>
+<ul class="ul-p">
+  <li class="li-p">${projectsArray[3].tecnologies[0]}</li>
+  <li class="li-p">${projectsArray[3].tecnologies[1]}</li>
+  <li class="li-p">${projectsArray[3].tecnologies[2]}</li>
+</ul>
+<button type="button" class="btn-p">See Project</button>
+</section>
+
+
+<section class="Project-5" id="project-5"> 
+ <h3 class="title-p">${projectsArray[4].title}</h3>
+<p class="p-Project">${projectsArray[4].paragraph}
+</p>
+<ul class="ul-p">
+  <li class="li-p">${projectsArray[4].tecnologies[0]}</li>
+  <li class="li-p">${projectsArray[4].tecnologies[1]}</li>
+  <li class="li-p">${projectsArray[4].tecnologies[2]}</li>
+</ul>
+<button type="button" class="btn-p">See Project</button>
+</section>
+
+<section class="Project-6" id="project-6"> 
+ <h3 class="title-p">${projectsArray[5].title}</h3>
+<p class="p-Project">${projectsArray[5].paragraph}
+</p>
+<ul class="ul-p">
+  <li class="li-p">${projectsArray[5].tecnologies[0]}</li>
+  <li class="li-p">${projectsArray[5].tecnologies[1]}</li>
+  <li class="li-p">${projectsArray[5].tecnologies[2]}</li>
+</ul>
+<button type="button" class="btn-p">See Project</button>
+</section>`;
+
+document.getElementById('project-1').style.backgroundImage = projectsArray[0].image;
+document.getElementById('project-2').style.backgroundImage = projectsArray[1].image;
+document.getElementById('project-3').style.backgroundImage = projectsArray[2].image;
+document.getElementById('project-4').style.backgroundImage = projectsArray[3].image;
+document.getElementById('project-5').style.backgroundImage = projectsArray[4].image;
+document.getElementById('project-6').style.backgroundImage = projectsArray[5].image;
