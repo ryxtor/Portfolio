83f324cf45e025af11f31fb84ec4139175900888
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-01-25 19:18:45 -0300

    Add JavaScript Objects

--- javascript.js
+++ javascript.js
@@ -12,3 +12,93 @@ document.getElementById('close-nav').onclick = closeNav;
 document.getElementById('close-nav1').onclick = closeNav;
 document.getElementById('close-nav2').onclick = closeNav;
 document.getElementById('close-nav3').onclick = closeNav;
+
+const project0 = {
+  title: 'Profesional Art<br>Printing Data',
+  paragraph: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+  image: 'src/bg-project.svg',
+  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+  text ever since the 1500s, when an unknown printer took a galley of type 
+  and scrambled it to make a type specimen book. It has survived not only 
+  five centuries, but also the leap into electronic typesetting, remaining essent`,
+  live: '#',
+  source: '#',
+};
+
+const project1 = {
+  title: 'Profesional Art<br>Printing Data',
+  paragraph: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+  image: 'src/bg-project.svg',
+  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+  text ever since the 1500s, when an unknown printer took a galley of type 
+  and scrambled it to make a type specimen book. It has survived not only 
+  five centuries, but also the leap into electronic typesetting, remaining essent`,
+  live: '#',
+  source: '#',
+};
+
+const project2 = {
+  title: 'Profesional Art<br>Printing Data',
+  paragraph: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+  image: 'src/bg-project.svg',
+  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+  text ever since the 1500s, when an unknown printer took a galley of type 
+  and scrambled it to make a type specimen book. It has survived not only 
+  five centuries, but also the leap into electronic typesetting, remaining essent`,
+  live: '#',
+  source: '#',
+};
+
+const project3 = {
+  title: 'Profesional Art<br>Printing Data',
+  paragraph: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+  image: 'src/bg-project.svg',
+  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+  text ever since the 1500s, when an unknown printer took a galley of type 
+  and scrambled it to make a type specimen book. It has survived not only 
+  five centuries, but also the leap into electronic typesetting, remaining essent`,
+  live: '#',
+  source: '#',
+};
+
+const project4 = {
+  title: 'Profesional Art<br>Printing Data',
+  paragraph: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+  image: 'src/bg-project.svg',
+  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+  text ever since the 1500s, when an unknown printer took a galley of type 
+  and scrambled it to make a type specimen book. It has survived not only 
+  five centuries, but also the leap into electronic typesetting, remaining essent`,
+  live: '#',
+  source: '#',
+};
+
+const project5 = {
+  title: 'Profesional Art<br>Printing Data',
+  paragraph: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+  image: 'src/bg-project.svg',
+  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+  text ever since the 1500s, when an unknown printer took a galley of type 
+  and scrambled it to make a type specimen book. It has survived not only 
+  five centuries, but also the leap into electronic typesetting, remaining essent`,
+  live: '#',
+  source: '#',
+};
