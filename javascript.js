8ce72343ca3a916d0c8ac1b05c51987ac3cad4f0
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-01-24 22:51:36 -0300

    Fix JavaScript linters

--- javascript.js
+++ javascript.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
   document.querySelector('#mobile-menu').style.height = '0%';
 }
+
+document.getElementById('open-menu').onclick = openNav;
+
+document.getElementById('close-nav').onclick = closeNav;
+document.getElementById('close-nav1').onclick = closeNav;
+document.getElementById('close-nav2').onclick = closeNav;
+document.getElementById('close-nav3').onclick = closeNav;
