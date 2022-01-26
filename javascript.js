fc56d73f666be1b9b6143dfcbe7ee028cc45fab7
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-01-26 17:10:01 -0300

    Fix linters errors and Add buttons id's

--- javascript.js
+++ javascript.js
@@ -159,15 +159,19 @@ function popup(num) {
     See Source <i class="fab fa-github darkicon" title="github icon"></i></button>
     </div>
   </div>`;
- 
+
   const startpopup = document.getElementById('popup');
   function erase() {
     while (startpopup.firstChild) {
       startpopup.removeChild(startpopup.lastChild);
     }
   }
-
-document.getElementById('recentWork').innerHTML = `
+  
+  document.getElementById('close-popup').addEventListener('click', () => {
+    erase();
+  });
+}
+  document.getElementById('recentWork').innerHTML = `
 <img class="img-works" width="327" height="237" alt="works image" src=${projectsArray[6].image}>
 <div class="works-info">
 <h3 class="works-title-2">${projectsArray[6].title}</h3>
@@ -178,10 +182,10 @@ document.getElementById('recentWork').innerHTML = `
   <li class="li2">${projectsArray[6].tecnologies[2]}</li>
   <li class="li2">${projectsArray[6].tecnologies[3]}</li>
 </ul>
-<button type="button" class="see-project-btn">See Project</button>
+<button type="button" id="btn-project-work" class="see-project-btn">See Project</button>
 </div>`;
 
-document.getElementById('projects').innerHTML = `
+  document.getElementById('projects').innerHTML = `
 <section class="Project-1" id="project-1"> 
  <h3 class="title-p">${projectsArray[0].title}</h3>
 <p class="p-Project">${projectsArray[0].paragraph}
@@ -191,7 +195,7 @@ document.getElementById('projects').innerHTML = `
   <li class="li-p">${projectsArray[0].tecnologies[1]}</li>
   <li class="li-p">${projectsArray[0].tecnologies[2]}</li>
 </ul>
-<button type="button" class="btn-p">See Project</button>
+<button type="button" id="btn-project-1" class="btn-p">See Project</button>
 </section>
 
  
@@ -204,7 +208,7 @@ document.getElementById('projects').innerHTML = `
   <li class="li-p">${projectsArray[1].tecnologies[1]}</li>
   <li class="li-p">${projectsArray[1].tecnologies[2]}</li>
 </ul>
-<button type="button" class="btn-p">See Project</button>
+<button type="button" id="btn-project-2" class="btn-p">See Project</button>
 </section>
 
  
@@ -217,7 +221,7 @@ document.getElementById('projects').innerHTML = `
   <li class="li-p">${projectsArray[2].tecnologies[1]}</li>
   <li class="li-p">${projectsArray[2].tecnologies[2]}</li>
 </ul>
-<button type="button" class="btn-p">See Project</button>
+<button type="button" id="btn-project-3" class="btn-p">See Project</button>
 </section>
 
 
@@ -230,7 +234,7 @@ document.getElementById('projects').innerHTML = `
   <li class="li-p">${projectsArray[3].tecnologies[1]}</li>
   <li class="li-p">${projectsArray[3].tecnologies[2]}</li>
 </ul>
-<button type="button" class="btn-p">See Project</button>
+<button type="button" id="btn-project-4" class="btn-p">See Project</button>
 </section>
 
 
@@ -243,7 +247,7 @@ document.getElementById('projects').innerHTML = `
   <li class="li-p">${projectsArray[4].tecnologies[1]}</li>
   <li class="li-p">${projectsArray[4].tecnologies[2]}</li>
 </ul>
-<button type="button" class="btn-p">See Project</button>
+<button type="button" id="btn-project-5" class="btn-p">See Project</button>
 </section>
 
 <section class="Project-6" id="project-6"> 
@@ -255,40 +259,37 @@ document.getElementById('projects').innerHTML = `
   <li class="li-p">${projectsArray[5].tecnologies[1]}</li>
   <li class="li-p">${projectsArray[5].tecnologies[2]}</li>
 </ul>
-<button type="button" class="btn-p">See Project</button>
+<button type="button" id="btn-project-6" class="btn-p">See Project</button>
 </section>`;
 
-document.getElementById('project-1').style.backgroundImage = projectsArray[0].image;
-document.getElementById('project-2').style.backgroundImage = projectsArray[1].image;
-document.getElementById('project-3').style.backgroundImage = projectsArray[2].image;
-document.getElementById('project-4').style.backgroundImage = projectsArray[3].image;
-document.getElementById('project-5').style.backgroundImage = projectsArray[4].image;
-document.getElementById('project-6').style.backgroundImage = projectsArray[5].image;
+  document.getElementById('project-1').style.backgroundImage = projectsArray[0].image;
+  document.getElementById('project-2').style.backgroundImage = projectsArray[1].image;
+  document.getElementById('project-3').style.backgroundImage = projectsArray[2].image;
+  document.getElementById('project-4').style.backgroundImage = projectsArray[3].image;
+  document.getElementById('project-5').style.backgroundImage = projectsArray[4].image;
+  document.getElementById('project-6').style.backgroundImage = projectsArray[5].image;
 
-document.getElementById('close-popup').addEventListener('click', () => {
-  erase();
-});
-}
+
+
 
 document.getElementById('btn-project-1').addEventListener('click', () => {
-popup(0);
+  popup(0);
 });
 document.getElementById('btn-project-2').addEventListener('click', () => {
-popup(1);
+  popup(1);
 });
 document.getElementById('btn-project-3').addEventListener('click', () => {
-popup(2);
+  popup(2);
 });
 document.getElementById('btn-project-4').addEventListener('click', () => {
-popup(3);
+  popup(3);
 });
 document.getElementById('btn-project-5').addEventListener('click', () => {
-popup(4);
+  popup(4);
 });
 document.getElementById('btn-project-6').addEventListener('click', () => {
-popup(5);
+  popup(5);
 });
 document.getElementById('btn-project-work').addEventListener('click', () => {
-popup(6);
+  popup(6);
 });
-
