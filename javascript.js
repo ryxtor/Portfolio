35da24c2e925b0b7add0ee864f0615aca9bd18f1
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-01-26 22:50:54 -0300

    Add loops to avoid repeat code and merge the array with the objects

--- javascript.js
+++ javascript.js
@@ -13,102 +13,6 @@ document.getElementById('close-nav1').onclick = closeNav;
 document.getElementById('close-nav2').onclick = closeNav;
 document.getElementById('close-nav3').onclick = closeNav;
 
-const project0 = {
-  title: 'Profesional Art Printing Data',
-  paragraph: `A daily selection of privately personalized reads;
-   no accounts or sign-ups required. has been the industry's standard`,
-  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'url(src/bg-project.svg)',
-  imagepopup: 'src/project-popup.svg',
-  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
-  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-  text ever since the 1500s, when an unknown printer took a galley of type 
-  and scrambled it to make a type specimen book. It has survived not only 
-  five centuries, but also the leap into electronic typesetting, remaining essent`,
-  live: '#',
-  source: '#',
-};
-
-const project1 = {
-  title: 'Profesional Art Printing Data',
-  paragraph: `A daily selection of privately personalized reads;
-   no accounts or sign-ups required. has been the industry's standard`,
-  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'url(src/bg-project.svg)',
-  imagepopup: 'src/project-popup.svg',
-  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
-  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-  text ever since the 1500s, when an unknown printer took a galley of type 
-  and scrambled it to make a type specimen book. It has survived not only 
-  five centuries, but also the leap into electronic typesetting, remaining essent`,
-  live: '#',
-  source: '#',
-};
-
-const project2 = {
-  title: 'Profesional Art Printing Data',
-  paragraph: `A daily selection of privately personalized reads;
-   no accounts or sign-ups required. has been the industry's standard`,
-  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'url(src/bg-project.svg)',
-  imagepopup: 'src/project-popup.svg',
-  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
-  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-  text ever since the 1500s, when an unknown printer took a galley of type 
-  and scrambled it to make a type specimen book. It has survived not only 
-  five centuries, but also the leap into electronic typesetting, remaining essent`,
-  live: '#',
-  source: '#',
-};
-
-const project3 = {
-  title: 'Profesional Art Printing Data',
-  paragraph: `A daily selection of privately personalized reads;
-   no accounts or sign-ups required. has been the industry's standard`,
-  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'url(src/bg-project.svg)',
-  imagepopup: 'src/project-popup.svg',
-  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
-  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-  text ever since the 1500s, when an unknown printer took a galley of type 
-  and scrambled it to make a type specimen book. It has survived not only 
-  five centuries, but also the leap into electronic typesetting, remaining essent`,
-  live: '#',
-  source: '#',
-};
-
-const project4 = {
-  title: 'Profesional Art Printing Data',
-  paragraph: `A daily selection of privately personalized reads;
-   no accounts or sign-ups required. has been the industry's standard`,
-  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'url(src/bg-project.svg)',
-  imagepopup: 'src/project-popup.svg',
-  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
-  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-  text ever since the 1500s, when an unknown printer took a galley of type 
-  and scrambled it to make a type specimen book. It has survived not only 
-  five centuries, but also the leap into electronic typesetting, remaining essent`,
-  live: '#',
-  source: '#',
-};
-
-const project5 = {
-  title: 'Profesional Art Printing Data',
-  paragraph: `A daily selection of privately personalized reads;
-   no accounts or sign-ups required. has been the industry's standard`,
-  tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
-  image: 'url(src/bg-project.svg)',
-  imagepopup: 'src/project-popup.svg',
-  popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
-  typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-  text ever since the 1500s, when an unknown printer took a galley of type 
-  and scrambled it to make a type specimen book. It has survived not only 
-  five centuries, but also the leap into electronic typesetting, remaining essent`,
-  live: '#',
-  source: '#',
-};
-
 const recentWorks = {
   image: 'src/Img%20Placeholder.svg',
   imagepopup: 'src/project-popup.svg',
@@ -129,14 +33,135 @@ const recentWorks = {
 };
 
 const projectsArray = [
-  project0,
-  project1,
-  project2,
-  project3,
-  project4,
-  project5,
-  recentWorks,
+  {
+    title: 'Profesional Art Printing Data',
+    paragraph: `A daily selection of privately personalized reads;
+     no accounts or sign-ups required. has been the industry's standard`,
+    tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+    image: 'url(src/bg-project.svg)',
+    imagepopup: 'src/project-popup.svg',
+    popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+    text ever since the 1500s, when an unknown printer took a galley of type 
+    and scrambled it to make a type specimen book. It has survived not only 
+    five centuries, but also the leap into electronic typesetting, remaining essent`,
+    live: '#',
+    source: '#',
+    class: 'Project-1',
+    id: 'project-1',
+    btnid: 'btn-project-1',
+  },
+
+  {
+    title: 'Profesional Art Printing Data',
+    paragraph: `A daily selection of privately personalized reads;
+     no accounts or sign-ups required. has been the industry's standard`,
+    tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+    image: 'url(src/bg-project.svg)',
+    imagepopup: 'src/project-popup.svg',
+    popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+    text ever since the 1500s, when an unknown printer took a galley of type 
+    and scrambled it to make a type specimen book. It has survived not only 
+    five centuries, but also the leap into electronic typesetting, remaining essent`,
+    live: '#',
+    source: '#',
+    class: 'Project-2',
+    id: 'project-2',
+    btnid: 'btn-project-2',
+  },
+
+  {
+    title: 'Profesional Art Printing Data',
+    paragraph: `A daily selection of privately personalized reads;
+     no accounts or sign-ups required. has been the industry's standard`,
+    tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+    image: 'url(src/bg-project.svg)',
+    imagepopup: 'src/project-popup.svg',
+    popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+    text ever since the 1500s, when an unknown printer took a galley of type 
+    and scrambled it to make a type specimen book. It has survived not only 
+    five centuries, but also the leap into electronic typesetting, remaining essent`,
+    live: '#',
+    source: '#',
+    class: 'Project-3',
+    id: 'project-3',
+    btnid: 'btn-project-3',
+  },
+
+  {
+    title: 'Profesional Art Printing Data',
+    paragraph: `A daily selection of privately personalized reads;
+     no accounts or sign-ups required. has been the industry's standard`,
+    tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+    image: 'url(src/bg-project.svg)',
+    imagepopup: 'src/project-popup.svg',
+    popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+    text ever since the 1500s, when an unknown printer took a galley of type 
+    and scrambled it to make a type specimen book. It has survived not only 
+    five centuries, but also the leap into electronic typesetting, remaining essent`,
+    live: '#',
+    source: '#',
+    class: 'Project-4',
+    id: 'project-4',
+    btnid: 'btn-project-4',
+  },
+
+  {
+    title: 'Profesional Art Printing Data',
+    paragraph: `A daily selection of privately personalized reads;
+     no accounts or sign-ups required. has been the industry's standard`,
+    tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+    image: 'url(src/bg-project.svg)',
+    imagepopup: 'src/project-popup.svg',
+    popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+    text ever since the 1500s, when an unknown printer took a galley of type 
+    and scrambled it to make a type specimen book. It has survived not only 
+    five centuries, but also the leap into electronic typesetting, remaining essent`,
+    live: '#',
+    source: '#',
+    class: 'Project-5',
+    id: 'project-5',
+    btnid: 'btn-project-5',
+  },
+
+  {
+    title: 'Profesional Art Printing Data',
+    paragraph: `A daily selection of privately personalized reads;
+     no accounts or sign-ups required. has been the industry's standard`,
+    tecnologies: ['HTML', 'Bootstrap', 'Ruby'],
+    image: 'url(src/bg-project.svg)',
+    imagepopup: 'src/project-popup.svg',
+    popupdescription: `Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+    text ever since the 1500s, when an unknown printer took a galley of type 
+    and scrambled it to make a type specimen book. It has survived not only 
+    five centuries, but also the leap into electronic typesetting, remaining essent`,
+    live: '#',
+    source: '#',
+    class: 'Project-6',
+    id: 'project-6',
+    btnid: 'btn-project-6',
+  },
 ];
+
+function closepopup() {
+  document.querySelector('html').style.overflow = 'hidden';
+  const startpopup = document.getElementById('popup');
+  function erase() {
+    while (startpopup.firstChild) {
+      startpopup.removeChild(startpopup.lastChild);
+    }
+    document.querySelector('html').style.overflow = 'scroll';
+  }
+
+  document.getElementById('close-popup').addEventListener('click', () => {
+    erase();
+  });
+}
 
 function popup(num) {
   document.getElementById('popup').innerHTML = `
@@ -164,135 +189,83 @@ function popup(num) {
     </div>
   </div>`;
 
-  document.querySelector('html').style.overflow = 'hidden';
-  const startpopup = document.getElementById('popup');
-  function erase() {
-    while (startpopup.firstChild) {
-      startpopup.removeChild(startpopup.lastChild);
-    }
-    document.querySelector('html').style.overflow = 'scroll';
-  }
-
-  document.getElementById('close-popup').addEventListener('click', () => {
-    erase();
-  });
+  closepopup();
 }
+
+function popuprecentwork() {
+  document.getElementById('popup').innerHTML = `
+  <div class="darkpage"></div>
+  <div class="popup-container" >
+    <button id="close-popup" class="close-pu" type="button">&times;</button>
+    <h3 class="works-title-2 title-pu">${recentWorks.title}</h3>
+    <ul class="ul-p ul-pu">  
+      <li class="li-p li-pu">${recentWorks.tecnologies[0]}</li>  
+      <li class="li-p li-pu">${recentWorks.tecnologies[1]}</li>  
+      <li class="li-p li-pu">${recentWorks.tecnologies[2]}</li>
+      <li class="li-p li-pu">${recentWorks.tecnologies[3]}</li>
+    </ul>
+    <div class="flex-popup">
+    <img class="popup-img" src=${recentWorks.imagepopup}
+    alt="popup image" width="311" height="220"></img>
+    <div class="popup-right">
+    <p class="p-Project p-popup">${recentWorks.popupdescription}</p>
+    <div class="popup-btn-container">
+    <button type="button" class="popup-btn" href=${recentWorks.live}>
+    See Live <i class="fas fa-external-link-alt darkicon"></i></button>
+    <button type="button" class="popup-btn btn-ml" href=${recentWorks.source}>
+    See Source <i class="fab fa-github darkicon" title="github icon"></i></button>
+    </div>
+    </div>
+    </div>
+  </div>`;
+
+  closepopup();
+}
+
 document.getElementById('recentWork').innerHTML = `
-<img class="img-works" width="327" height="237" alt="works image" src=${projectsArray[6].image}>
+<img class="img-works" width="327" height="237" alt="works image" src=${recentWorks.image}>
 <div class="works-info">
-<h3 class="works-title-2">${projectsArray[6].title}</h3>
-<p class="p-work">${projectsArray[6].paragraph}</p>
+<h3 class="works-title-2">${recentWorks.title}</h3>
+<p class="p-work">${recentWorks.paragraph}</p>
 <ul>
-  <li class="li2">${projectsArray[6].tecnologies[0]}</li>
-  <li class="li2">${projectsArray[6].tecnologies[1]}</li>
-  <li class="li2">${projectsArray[6].tecnologies[2]}</li>
-  <li class="li2">${projectsArray[6].tecnologies[3]}</li>
+  <li class="li2">${recentWorks.tecnologies[0]}</li>
+  <li class="li2">${recentWorks.tecnologies[1]}</li>
+  <li class="li2">${recentWorks.tecnologies[2]}</li>
+  <li class="li2">${recentWorks.tecnologies[3]}</li>
 </ul>
-<button type="button" id="btn-project-work" class="see-project-btn">See Project</button>
+<button type="button" id="btn-project-works" class="see-project-btn">See Project</button>
 </div>`;
 
-document.getElementById('projects').innerHTML = `
-<section class="Project-1" id="project-1"> 
- <h3 class="title-p">${projectsArray[0].title}</h3>
-<p class="p-Project">${projectsArray[0].paragraph}
+function writeProjects(item) {
+  const card = `
+<section class=${item.class} id=${item.id}> 
+ <h3 class="title-p">${item.title}</h3>
+<p class="p-Project">${item.paragraph}
 </p>
 <ul class="ul-p">
-  <li class="li-p">${projectsArray[0].tecnologies[0]}</li>
-  <li class="li-p">${projectsArray[0].tecnologies[1]}</li>
-  <li class="li-p">${projectsArray[0].tecnologies[2]}</li>
+  <li class="li-p">${item.tecnologies[0]}</li>
+  <li class="li-p">${item.tecnologies[1]}</li>
+  <li class="li-p">${item.tecnologies[2]}</li>
 </ul>
-<button type="button" id="btn-project-1" class="btn-p">See Project</button>
-</section>
-
- 
-<section class="Project-2" id="project-2"> 
- <h3 class="title-p">${projectsArray[1].title}</h3>
-<p class="p-Project">${projectsArray[1].paragraph}
-</p>
-<ul class="ul-p">
-  <li class="li-p">${projectsArray[1].tecnologies[0]}</li>
-  <li class="li-p">${projectsArray[1].tecnologies[1]}</li>
-  <li class="li-p">${projectsArray[1].tecnologies[2]}</li>
-</ul>
-<button type="button" id="btn-project-2" class="btn-p">See Project</button>
-</section>
-
- 
-<section class="Project-3" id="project-3"> 
- <h3 class="title-p">${projectsArray[2].title}</h3>
-<p class="p-Project">${projectsArray[2].paragraph}
-</p>
-<ul class="ul-p">
-  <li class="li-p">${projectsArray[2].tecnologies[0]}</li>
-  <li class="li-p">${projectsArray[2].tecnologies[1]}</li>
-  <li class="li-p">${projectsArray[2].tecnologies[2]}</li>
-</ul>
-<button type="button" id="btn-project-3" class="btn-p">See Project</button>
-</section>
-
-
-<section class="Project-4" id="project-4"> 
- <h3 class="title-p">${projectsArray[3].title}</h3>
-<p class="p-Project">${projectsArray[3].paragraph}
-</p>
-<ul class="ul-p">
-  <li class="li-p">${projectsArray[3].tecnologies[0]}</li>
-  <li class="li-p">${projectsArray[3].tecnologies[1]}</li>
-  <li class="li-p">${projectsArray[3].tecnologies[2]}</li>
-</ul>
-<button type="button" id="btn-project-4" class="btn-p">See Project</button>
-</section>
-
-
-<section class="Project-5" id="project-5"> 
- <h3 class="title-p">${projectsArray[4].title}</h3>
-<p class="p-Project">${projectsArray[4].paragraph}
-</p>
-<ul class="ul-p">
-  <li class="li-p">${projectsArray[4].tecnologies[0]}</li>
-  <li class="li-p">${projectsArray[4].tecnologies[1]}</li>
-  <li class="li-p">${projectsArray[4].tecnologies[2]}</li>
-</ul>
-<button type="button" id="btn-project-5" class="btn-p">See Project</button>
-</section>
-
-<section class="Project-6" id="project-6"> 
- <h3 class="title-p">${projectsArray[5].title}</h3>
-<p class="p-Project">${projectsArray[5].paragraph}
-</p>
-<ul class="ul-p">
-  <li class="li-p">${projectsArray[5].tecnologies[0]}</li>
-  <li class="li-p">${projectsArray[5].tecnologies[1]}</li>
-  <li class="li-p">${projectsArray[5].tecnologies[2]}</li>
-</ul>
-<button type="button" id="btn-project-6" class="btn-p">See Project</button>
+<button type="button" id=${item.btnid} class="btn-p">See Project</button>
 </section>`;
+  return card;
+}
 
-document.getElementById('project-1').style.backgroundImage = projectsArray[0].image;
-document.getElementById('project-2').style.backgroundImage = projectsArray[1].image;
-document.getElementById('project-3').style.backgroundImage = projectsArray[2].image;
-document.getElementById('project-4').style.backgroundImage = projectsArray[3].image;
-document.getElementById('project-5').style.backgroundImage = projectsArray[4].image;
-document.getElementById('project-6').style.backgroundImage = projectsArray[5].image;
+projectsArray.forEach((project) => {
+  const elementss = document.createElement('div');
+  elementss.innerHTML = writeProjects(project);
+  document.getElementById('projects').appendChild(elementss);
+});
 
-document.getElementById('btn-project-1').addEventListener('click', () => {
-  popup(0);
+projectsArray.forEach((image, i) => { document.getElementById(`project-${i + 1}`).style.backgroundImage = projectsArray[i].image; });
+
+projectsArray.forEach((project, i) => {
+  document.getElementById(`btn-project-${i + 1}`).addEventListener('click', () => {
+    popup(i);
+  });
 });
-document.getElementById('btn-project-2').addEventListener('click', () => {
-  popup(1);
-});
-document.getElementById('btn-project-3').addEventListener('click', () => {
-  popup(2);
-});
-document.getElementById('btn-project-4').addEventListener('click', () => {
-  popup(3);
-});
-document.getElementById('btn-project-5').addEventListener('click', () => {
-  popup(4);
-});
-document.getElementById('btn-project-6').addEventListener('click', () => {
-  popup(5);
-});
-document.getElementById('btn-project-work').addEventListener('click', () => {
-  popup(6);
+
+document.getElementById('btn-project-works').addEventListener('click', () => {
+  popuprecentwork();
 });
